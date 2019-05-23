b4ba6f0ebf0003d3e993dc9685460c54d459d30c
ngthienhung277 <44661168+ngthienhung277@users.noreply.github.com>
2019-05-23 15:34:33 +0700

    Update HelloWorld.java
    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 	public static void main( String [] args ){
 		System.out.println("hello world!");
 		System.out.println(1+1+3+4+5+5+9);
-                System.out.println(1+1+3+4+5+5);
+                System.out.println(1+1+3+4+5+9);
 	}
 }
