3e284154a0ceae4ec6f793900b8f4d2187ad32aa
Phuc Truong <ptruong8@dxc.com>
2019-05-27 09:02:32 +0700

    revert

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld{
 	public static void main( String [] args ){
 		System.out.println("hello world!");
 		System.out.println(1+1+3+4+5+5+9);
+<<<<<<< HEAD
                 System.out.println(1+1+3+4+5+5+5+5+69);
+=======
+                System.out.println(1+1+3+4+5+5);
+>>>>>>> parent of 420363e... Update HelloWorld.java
 	}
 }
