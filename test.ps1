49623c1a8036be26f8605823639816a2ee5c43f0
baoduy11700 <50854237+baoduy11700@users.noreply.github.com>
2019-06-11 15:19:01 +0700

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "Hello World !"
+$linuxpath = '/var/lib/jenkins/workspace/'
+$number = Get-Command | Measure-Object | Select-Object -Property Count
+New-item -Path $linuxpath -Name Test -Value $number
+cd $linuxpath
