51810711551a47a141c7806ce3f42b88cc9a5050
baoduy11700 <50854237+baoduy11700@users.noreply.github.com>
2019-06-11 15:20:29 +0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "Hello World !"
 $linuxpath = '/var/lib/jenkins/workspace/'
 $number = Get-Command | Measure-Object | Select-Object -Property Count
-New-item -Path $linuxpath -Name Test -Value $number
+New-item -Path $linuxpath -Name NumberofCommand -Value $number
 cd $linuxpath
