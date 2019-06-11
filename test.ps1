de632e7a2bbaa77a6b7e8cc1fefab69f6c0cd63b
baoduy11700 <50854237+baoduy11700@users.noreply.github.com>
2019-06-11 15:31:03 +0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,9 @@
 ﻿Write-Host "Hello World !"
 $linuxpath = '/var/lib/jenkins/workspace/'
 $number = Get-Command | Measure-Object | Select-Object -Property Count
-New-item -Path $linuxpath -Name NumberofCommand -Value $number
-cd $linuxpath
+if(Test-Path $linuxpath/NumberofCommand.txt){
+    Remove-Item -Path $linuxpath/NumberofCommand.txt
+    New-item -Path $linuxpath -Name NumberofCommand.txt -Value $number
+}else{
+New-item -Path $linuxpath -Name NumberofCommand.txt -Value $number}
+Get-Content -Path $linuxpath/NumberofCommand.txt
