b1d4321d4aabeaa989eb7250eadbb1ca7736f1e8
baoduy11700 <50854237+baoduy11700@users.noreply.github.com>
2019-06-12 09:58:48 +0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,7 +4,7 @@ $number = Get-Command | Measure-Object | Select-Object -Property Count | Out-Str
 [string]$textfile = "Chi tiết phiên bản của powershell là : $($PSVersionTable | Out-String) Tổng số câu lệnh là : $($number)"
 if(Test-Path $linuxpath/NumberofCommand.txt){
     Remove-Item -Path $linuxpath/NumberofCommand.txt
-    New-item -Path $linuxpath -Name NumberofCommand.txt -Value $number
+    New-item -Path $linuxpath -Name NumberofCommand.txt -Value $textfile
 }else{
-New-item -Path $linuxpath -Name NumberofCommand.txt -Value $number}
+New-item -Path $linuxpath -Name NumberofCommand.txt -Value $textfile}
 Get-Content -Path $linuxpath/NumberofCommand.txt
