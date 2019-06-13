7a2d00d37c93dabaa86371180dc9223140b3e8ae
baoduy11700 <50854237+baoduy11700@users.noreply.github.com>
2019-06-13 14:58:10 +0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = "Stop"
 Write-Host "Hello World !"
-$linuxpath = '/var/'
+$linuxpath = '/home/duyvmagent/workspace/'
 $number = Get-Command | Measure-Object | Select-Object -Property Count | Out-String
 [string]$textfile = "Chi tiết phiên bản của powershell là : $($PSVersionTable | Out-String) Tổng số câu lệnh là : $($number)"
 if(Test-Path $linuxpath/NumberofCommand.txt){
